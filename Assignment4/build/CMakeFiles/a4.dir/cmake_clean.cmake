ad41df974b5f59b0059cdc07f85e49f6653637bc
jelly <jamesla200210@gmail.com>
2024-05-22 16:52:25 +1200

    assignment 4 CORE part1 and part2

---
+++ Assignment4/build/CMakeFiles/a4.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/a4.dir/FeatureExtractionAndMatching.cpp.o"
+  "CMakeFiles/a4.dir/FeatureExtractionAndMatching.cpp.o.d"
+  "a4"
+  "a4.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/a4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
